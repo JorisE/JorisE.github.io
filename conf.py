6bc336a792c60bf55b4c743b4d8413f89ed7ac73
Joris Engbers <info@jorisengbers.nl>
2016-09-27 22:08:34 +0200

    Nikola auto commit.
    Nikola version: 7.8.0

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "JHA Engbers"  # (translatable)
 BLOG_TITLE = "Le Website"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://joris.engbers.info/"
+SITE_URL = "https://jorise.github.io"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://joris.engbers.info/"
